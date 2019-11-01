02737473544afa9dcb9b1f672ee1edbc80f620ce
pidwid <luckyshukla765@gmail.com>
2019-11-01 08:47:04 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:devfest_demo_app/utils/devfest.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async{
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent),);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  
+  Devfest.prefs = await SharedPreferences.getInstance();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Dev Fest App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
